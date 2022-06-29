6a0fb4e8619146c80dcbbb85bf71383ccdb27aa1
Alona Zorina <alozorina@gmail.com>
2022-06-29 18:51:52 +0200

    add new model for password and email update

---
+++ BLL/Models/UserCreds.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.Models
+{
+    public class UserCreds
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password), StringLength(32, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
