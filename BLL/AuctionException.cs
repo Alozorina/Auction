ee88cb5e30b2eed2fa30d6b7b331c84409bffa02
Alona Zorina <alozorina@gmail.com>
2022-07-05 15:04:06 +0200

    added new class constructors

--- BLL/AuctionException.cs
+++ BLL/AuctionException.cs
@@ -4,5 +4,8 @@ namespace BLL
 {
     public class AuctionException : Exception
     {
+        public AuctionException() { }
+        public AuctionException(string message) : base(message) { }
+        public AuctionException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
