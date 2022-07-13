bdc7f740779f555c7a5a7da937fcca4fac6c6a3f
Alona Zorina <alozorina@gmail.com>
2022-07-13 17:49:12 +0200

    add Quartz

--- Auction/Program.cs
+++ Auction/Program.cs
@@ -1,6 +1,8 @@
+using Auction.Scheduler;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Quartz;
 
 namespace Auction
 {
@@ -21,6 +23,32 @@ namespace Auction
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Add the required Quartz.NET services
+                services.AddQuartz(q =>
+                {
+                    q.UseMicrosoftDependencyInjectionScopedJobFactory();
+
+                    // Create a "key" for the job
+                    var jobKey = new JobKey("HelloWorldJob");
+
+                    // Register the job with the DI container
+                    q.AddJob<HelloWorldJob>(opts => opts.WithIdentity(jobKey));
+
+                    // Create a trigger for the job
+                    q.AddTrigger(opts => opts
+                        .ForJob(jobKey) // link to the HelloWorldJob
+                        .WithIdentity("HelloWorldJob-trigger") // give the trigger a unique name
+                        .WithCronSchedule("0/5 * * * * ?")); // run every 5 seconds
+
+                });
+                services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+                // Add the Quartz.NET hosted service
+                services.AddQuartzHostedService(
+                    q => q.WaitForJobsToComplete = true);
             });
     }
 }
---
+++ Auction/Scheduler/HelloWorldJob.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+namespace Auction.Scheduler
+{
+    [DisallowConcurrentExecution]
+    public class HelloWorldJob : IJob
+    {
+        private readonly ILogger<HelloWorldJob> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HelloWorldJob(ILogger<HelloWorldJob> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
