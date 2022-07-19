311ca3a91fd62d5c0755c5e9d62d104c22fe1377
Alona Zorina <alozorina@gmail.com>
2022-07-19 15:36:24 +0300

    add scheduler

---
+++ Auction/Scheduler/UpdateItemCloseStatusJob.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace Auction.Scheduler
+{
+    [DisallowConcurrentExecution]
+    public class UpdateItemCloseStatusJob : IJob
+    {
+        private readonly ILogger<UpdateItemCloseStatusJob> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateItemCloseStatusJob(ILogger<UpdateItemCloseStatusJob> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var items = await _unitOfWork.ItemRepository.FindByPredicateAsync(i => i.StatusId == 4);
+
+            foreach (var item in items)
+            {
+                if (item.EndSaleDate < DateTime.Now)
+                {
+                    item.StatusId = 5;
+                    _logger.LogInformation($"Item {item.Name} with ID {item.Id} is Closed");
+                }
+            }
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ Auction/Scheduler/UpdateItemOpenStatusJob.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace Auction.Scheduler
+{
+    public class UpdateItemOpenStatusJob : IJob
+    {
+        private readonly ILogger<UpdateItemOpenStatusJob> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateItemOpenStatusJob(ILogger<UpdateItemOpenStatusJob> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var items = await _unitOfWork.ItemRepository.FindByPredicateAsync(i => i.StatusId == 2);
+
+            foreach (var item in items)
+            {
+                if (item.StartSaleDate < DateTime.Now)
+                {
+                    item.StatusId = 4;
+                    _logger.LogInformation($"Item {item.Name} with ID {item.Id} is Open");
+                }
+            }
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
