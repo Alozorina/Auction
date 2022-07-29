a8b356171906aa48df9e92a1faa5b222990c29ad
Alona Zorina <alozorina@gmail.com>
2022-07-29 18:28:04 +0300

    add new model for adding new item

--- BLL/Models/ItemCreateNewEntity.cs
+++ BLL/Models/ItemCreateNewEntity.cs
@@ -1,35 +1,31 @@
 ﻿using DAL.CustomValidationAttributes;
 using DAL.Entities;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace BLL.Models
 {
-    public class ItemPublicInfo
+    public class ItemCreateNewEntity
     {
-        [Required]
-        public int Id { get; set; }
         [Required, StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
         [Required, StringLength(80, MinimumLength = 5)]
         public string CreatedBy { get; set; }
         [Required]
         public int OwnerId { get; set; }
-        public int? BuyerId { get; set; }
         [MaxLength(1000)]
         public string Description { get; set; }
         [Required, DataType(DataType.Currency), Range(double.Epsilon, double.MaxValue, ErrorMessage = "Please enter a value bigger than 0")]
         public decimal StartingPrice { get; set; }
-        [DataType(DataType.Currency)]
-        public decimal CurrentBid { get; set; }
         [Required, StartSaleDate, DataType(DataType.Date)]
         public DateTime StartSaleDate { get; set; }
         [Required, EndSaleDate, DataType(DataType.Date)]
         public DateTime EndSaleDate { get; set; }
-        public ICollection<ItemCategory> ItemCategories { get; set; }
-        public virtual ICollection<ItemPhoto> ItemPhotos { get; set; }
+        public ICollection<ItemCategory> ItemCategoryNames { get; set; }
         [Required]
-        public string Status { get; set; }
+        public virtual List<IFormFile> ItemFormFilePhotos { get; set; }
     }
 }
