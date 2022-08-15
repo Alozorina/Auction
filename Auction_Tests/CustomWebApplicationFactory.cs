46df4b9d6607b9680c40ead3906d192b152385d3
Alona <alona.zorina@nure.ua>
2022-08-16 01:40:07 +0300

    add test user config

---
+++ Auction_Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,58 @@
+﻿using Auction;
+using DAL.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auction_Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                RemoveLibraryDbContextRegistration(services);
+
+                var serviceProvider = GetInMemoryServiceProvider();
+
+                services.AddDbContextPool<AuctionDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.Empty.ToString());
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                using (var scope = services.BuildServiceProvider().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+
+                    UnitTestHelper.SeedData(context);
+                }
+            });
+        }
+
+        private static ServiceProvider GetInMemoryServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+        }
+
+        private static void RemoveLibraryDbContextRegistration(IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<AuctionDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+    }
+}
---
+++ Auction_Tests/UnitTestHelper.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using BLL;
+using DAL.Data;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Auction_Tests
+{
+    public static class UnitTestHelper
+    {
+        public static DbContextOptions<AuctionDbContext> GetUnitTestDbOptions()
+        {
+            var options = new DbContextOptionsBuilder<AuctionDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using (var context = new AuctionDbContext(options))
+            {
+                SeedData(context);
+            }
+
+            return options;
+        }
+
+        public static IMapper CreateMapperProfile()
+        {
+            var myProfile = new AutomapperProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+
+            return new Mapper(configuration);
+        }
+
+        public static void SeedData(AuctionDbContext context)
+        {
+            context.Users.AddRange(
+                new User { Id = 1, FirstName = "Name1", LastName = "Surname1", Email = "user1@mm.com", 
+                           BirthDate = new DateTime(1980, 5, 25), Password = "password1", RoleId = 1});
+            context.SaveChanges();
+        }
+    }
+}
