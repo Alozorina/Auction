267831dcd9d62259c1342f3cd22c0545080daafc
Alona <alona.zorina@nure.ua>
2022-09-02 23:32:07 +0300

    delete RoleId

--- BLL/Models/UserRegistrationModel.cs
+++ BLL/Models/UserRegistrationModel.cs
@@ -15,11 +15,5 @@ namespace BLL.Models
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password), StringLength(32, MinimumLength = 6)]
         public string Password { get; set; }
-
-        public int RoleId { get; }
-        public UserRegistrationModel()
-        {
-            RoleId = 1;
-        }
     }
 }
