ec999c7b7304bbf470c986354a7327cc9ee2e125
Alona Zorina <alozorina@gmail.com>
2022-10-27 03:47:21 +0300

    added GetUserIdFromToken()

--- Auction/Middleware/ITokenManager.cs
+++ Auction/Middleware/ITokenManager.cs
@@ -7,6 +7,7 @@ namespace Auction.Middleware
         bool AddTokenToCache(string token);
         string GetCurrentToken();
         string GetStringToken(User user);
+        string GetUserIdFromToken();
         void InvalidateCurrentToken();
         void InvalidateToken(string token);
         bool IsActive(string token);
